c6c5ae05e5a41222a9d3093bd03cabd2673a2911
Bas van den Berg <BarabasGitHub@users.noreply.github.com>
2020-03-21 17:50:20 +0100

    Fixed build

--- build.zig
+++ build.zig
@@ -1,30 +1,42 @@
-const build_lib = @import("std").build;
+const std = @import("std");
+const build_lib = std.build;
 const Builder = build_lib.Builder;
+const builtin = std.builtin;
 
-const GLFWDependencies = [_][]const u8{ "c", "user32", "gdi32", "shell32" };
-const vulkan_library = "E:/Libraries/VulkanSDK/1.1.121.2/Lib/vulkan-1";
-const glfw_library = "E:/Libraries/glfw/build/src/Release/glfw3";
+const GLFWDependencies = [_][]const u8{
+    "c",
+    "user32",
+    "gdi32",
+    "shell32",
+};
+const vulkan_library = "E:/Libraries/VulkanSDK/1.1.121.2/Lib/vulkan-1.lib";
+const glfw_library = "E:/Libraries/glfw/build/src/Release/glfw3.lib";
 
 fn linkVulkanGLFWAndDependencies(step: *build_lib.LibExeObjStep) void {
-    step.linkSystemLibrary(vulkan_library);
-    step.linkSystemLibrary(glfw_library);
+    step.addObjectFile(vulkan_library);
+    step.addObjectFile(glfw_library);
 
-    step.linkSystemLibrary("c");
-    step.linkSystemLibrary("user32");
-    step.linkSystemLibrary("gdi32");
-    step.linkSystemLibrary("shell32");
+    for (GLFWDependencies) |dep| {
+        step.linkSystemLibrary(dep);
+    }
+}
+
+pub fn doCommonStuff(lib_or_tests: var, build_mode: builtin.Mode) void {
+    lib_or_tests.addPackagePath("ZigZag", "../ZigZag/main.zig");
+    lib_or_tests.addLibPath("C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.25.28610/lib/x64");
+    lib_or_tests.setBuildMode(build_mode);
+    linkVulkanGLFWAndDependencies(lib_or_tests);
+
 }
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const lib = b.addStaticLibrary("ZigVulkan", "main.zig");
-    linkVulkanGLFWAndDependencies(lib);
-    lib.setBuildMode(mode);
+    doCommonStuff(lib, mode);
     lib.install();
 
     var main_tests = b.addTest("main.zig");
-    linkVulkanGLFWAndDependencies(main_tests);
-    main_tests.setBuildMode(mode);
+    doCommonStuff(main_tests, mode);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
